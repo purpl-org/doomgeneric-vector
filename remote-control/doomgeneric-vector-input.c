4445912ad37ee4dbdd40515f328dd103f9da1ad5
ekeleze <ekeleze@ekeleze.net>
2026-01-14 17:39:46 -0600

    more logging 2

--- remote-control/doomgeneric-vector-input.c
+++ remote-control/doomgeneric-vector-input.c
@@ -120,6 +120,8 @@ int main(int argc, char *argv[]) {
             if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
                 unsigned char key = sdl_to_doom(e.key.keysym.sym);
                 if (key) {
+		    printf("%c\n", key);
+
                     unsigned char packet[2] = {
                         e.type == SDL_KEYDOWN ? 1 : 0,
                         key
