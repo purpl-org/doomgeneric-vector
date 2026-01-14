4c8246114adcd647eb7ccc0e25c80fddbe936478
ekeleze <ekeleze@ekeleze.net>
2026-01-14 01:01:17 -0600

    stupid git 2

---
+++ doomgeneric/main.c
@@ -0,0 +1,11 @@
+//
+// Created by ekeleze on 1/13/26.
+//
+
+void doomgeneric_Create(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+    doomgeneric_Create(argc, argv);
+    return 0;
+}
